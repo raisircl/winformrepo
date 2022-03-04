51ae5322207f39a262a57eee5204934bb149c03a
Rai Singh <rai.verma@live.com>
2022-03-04 18:34:31 +0530

    day1

---
+++ WindowsFormsDay1/WindowsFormsDay1/FrmWelcome.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDay1
+{
+    public partial class FrmWelcome : Form
+    {
+        public FrmWelcome()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOk_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello {txtName.Text}","Greeting",MessageBoxButtons.OKCancel);
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void FrmWelcome_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(MessageBox.Show("Area you sure to close?","Close Confirmation",MessageBoxButtons.YesNo,MessageBoxIcon.Question)==DialogResult.Yes)
+            {
+                e.Cancel = false;
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
