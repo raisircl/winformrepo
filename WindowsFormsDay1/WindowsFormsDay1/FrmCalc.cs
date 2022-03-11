f09b6115ce8fba06628ca2ca9d3734f8776254af
Rai Singh <rai.verma@live.com>
2022-03-11 15:23:44 +0530

    common event handler

---
+++ WindowsFormsDay1/WindowsFormsDay1/FrmCalc.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDay1
+{
+    public partial class FrmCalc : Form
+    {
+        double n1, n2, r;
+        double temp;
+        string op;
+        public FrmCalc()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = button1.Text;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = button3.Text;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = button2.Text;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = button4.Text;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = button5.Text;
+        }
+
+        private void commonbtn_click(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            textBox1.Text = btn.Text;
+        }
+    }
+}
---
+++ WindowsFormsDay1/WindowsFormsDay1/FrmEventDemo.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsDay1
+{
+    public partial class FrmEventDemo : Form
+    {
+        Button button;
+        public FrmEventDemo()
+        {
+            InitializeComponent();
+
+            button = new Button();
+            button.Width = 100;
+            button.Height = 50;
+            button.Text = "Click Me";
+            button.Top = 10;
+            button.Left = 10;
+            this.Controls.Add(button);
+            button.Click += Button_Click;
+        }
+
+        private void Button_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello from click me");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void FrmEventDemo_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.X + "," + e.Y;
+        }
+    }
+}
